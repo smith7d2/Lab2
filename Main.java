f61d67f2391fb00252bd221a7a9e1b87e9cade8a
Danielle Smith <dsmith18@sjastudents.org>
2020-09-09 04:20:19 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,31 @@
+;class Main {
+  public static void main(String[] args) {
+    System.out.println("M&M Color Counts");
+    int total = 55 * 10;
+    double blue = total * .24;
+    double brown = total * .13;
+    double green = total * .16;
+    double orange = total * .20;
+    double red = total * .13;
+    double yellow = total *.14;
+
+System.out.println("blue: " + blue); 
+System.out.print("brown:  " + brown); 
+System.out.println("green: " + green);
+System.out.println("orange: " + orange);
+System.out.println("red: " + red);
+System.out.println("yellow: " + yellow);
+
+double sum("blue + brown + green + orange + red + yellow ");
+System.out.println("Sum: " + sum);
+if (blue > brown & green > orange)
+System.out.println("Blue over Brown and Green over Orange");
+if (brown <= red)
+System.out.println("Brown is less than or equal to Red");
+if (sum == red)
+System.out.println("Numbers match");
+
+
+//code doesn't work
+  }
+}
